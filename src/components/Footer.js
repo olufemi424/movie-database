8d1b05029ff2d4228cd912b95418c7c9f7c144db
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2018-12-19 00:42:59 -0500

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,14 @@ import styled from "styled-components";
 const Footer = () => {
   return (
     <FooterStyle>
-      <p>Copyright &copy; Movie DataBase 2018</p>
+      <p>
+        Copyright &copy; Movie DataBase 2018 by{" "}
+        <span>
+          <a className="light" href="http//olufemiaf.com">
+            olufemiaf {`🤓`}
+          </a>
+        </span>
+      </p>
     </FooterStyle>
   );
 };
@@ -16,4 +23,11 @@ const FooterStyle = styled.div`
   height: 60px;
   padding: 10px;
   color: #fff;
+  .light {
+    font-weight: 200;
+    color: white;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+  }
 `;
