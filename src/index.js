40963c2bae77a2189d21a8fdf26ea5090f1440d0
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-05-22 14:08:01 -0500

    redux chome extention comment-out

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ import * as serviceWorker from "./serviceWorker";
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
